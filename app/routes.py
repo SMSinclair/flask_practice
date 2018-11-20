71a10b688b6979b495cd16b639ef3afd79b08824
Paul Kutrich <pkutrich@gmail.com>
2018-11-20 13:15:25 -0700

    Official Flask tutorial with some additions
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   app/__init__.py
    	new file:   app/routes.py
    	new file:   data.db
    	new file:   microblog.py

---
+++ app/routes.py
@@ -0,0 +1,89 @@
+from app import app
+import sqlite3 as sq3
+from sqlite3 import Error
+from flask import jsonify, request
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+def create_connection(database):
+    try:
+        conn = sq3.connect(database)
+        cur = conn.cursor()
+        return cur
+    except Error as e:
+        print(e)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    # TODO html for index page.
+    return 'Welcome to The Music Info Place'
+
+
+@app.route('/music/artists/all', methods=['GET'])
+def artists_all():
+    sql = 'SELECT name FROM artists'
+    cur = create_connection('data.db')
+    result = cur.execute(sql).fetchall()
+    return jsonify(result)
+
+
+@app.route('/music/artists', methods=['GET'])
+def artist_albums():
+    query_parameters = request.args
+    query = query_parameters.get('name')
+    sql = """
+    SELECT title
+    FROM albums
+    INNER JOIN  artists ON artists.ArtistId = albums.ArtistId
+    WHERE  artists.Name = ?;"""
+
+    cur = create_connection('data.db')
+    result = cur.execute(sql, ('{}'.format(query),)).fetchall()
+    if len(result):
+        return jsonify(result)
+    else:
+        return 'Sorry! We don\'t have info on that artist.'
+
+
+@app.route('/music/albums/all', methods=['GET'])
+def albums_all():
+    sql = """
+    SELECT Title FROM albums"""
+    cur = create_connection('data.db')
+    result = cur.execute(sql).fetchall()
+    return jsonify(result)
+
+
+@app.route('/music/albums', methods=['GET'])
+def album_tracks():
+    query_params = request.args
+    query = query_params.get('title')
+    sql = """
+    SELECT tracks.GenreId, name
+    FROM tracks
+    INNER JOIN albums ON albums.AlbumId = tracks.AlbumId
+    WHERE albums.Title = ?;"""
+
+    cur = create_connection('data.db')
+    result = cur.execute(sql, ('{}'.format(query),)).fetchall()
+    if len(result):
+        return jsonify(result)
+    else:
+        return 'Sorry! We don\'t have info on that album.'
+
+
+@app.route('/music/tracks/all', methods=['GET'])
+def tracks_all():
+    sql = """
+    SELECT name FROM tracks"""
+    cur = create_connection('data.db')
+    result = cur.execute(sql).fetchall()
+    return jsonify(result)
